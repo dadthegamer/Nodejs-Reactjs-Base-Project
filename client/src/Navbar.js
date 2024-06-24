70e0cbb9268bccb558fdfcb0273786814ecb2fbb
Craig Bailey <bailey_craig@me.com>
2024-06-24 15:16:35 -0500

    changed contact to open default mail app to send email

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -11,57 +11,50 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 export default function Navbar() {
     return (
         <Box
-            sx={
-                {
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    padding: '1rem',
-                    bgcolor: 'primary.main',
-                    color: 'primary.contrastText',
-                    position: 'fixed',
-                    top: 0,
-                    zIndex: 1000,
-                    width: '100%',
-                    height: '3.5rem',
-                    overflow: 'hidden',
-                    fontSize: '1rem',
-                    fontWeight: 700,
-                }
-            }>
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '1rem',
+                bgcolor: 'primary.main',
+                color: 'primary.contrastText',
+                position: 'fixed',
+                top: 0,
+                zIndex: 1000,
+                width: '100%',
+                height: '3.5rem',
+                overflow: 'hidden',
+                fontSize: '1rem',
+                fontWeight: 700,
+            }}
+        >
             <Box
-                sx={
-                    {
-                        display: 'flex',
-                        alignItems: 'center',
-                        marginLeft: '2rem',
-                        gap: '1rem',
-                    }
-                }
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    marginLeft: '2rem',
+                    gap: '1rem',
+                }}
             >
                 <Avatar src='/profile_pic.png' alt='Craig Bailey' sx={{ width: 50, height: 50, border: '2px solid black' }} />
                 <Typography variant='h7'>CRAIG BAILEY</Typography>
             </Box>
             <Box
-                sx={
-                    {
-                        display: 'flex',
-                        gap: '2rem',
-                    }
-                }
+                sx={{
+                    display: 'flex',
+                    gap: '2rem',
+                }}
             >
                 <Link href='/' color='inherit' underline='none'>ABOUT</Link>
                 <Link href='/portfolio' color='inherit' underline='none'>PORTFOLIO</Link>
-                <Link href='/contact' color='inherit' underline='none'>CONTACT</Link>
+                <Link href='mailto:bailey_craig@me.com' color='inherit' underline='none'>CONTACT</Link>
             </Box>
             <Box
-            sx={
-                {
+                sx={{
                     display: 'flex',
                     gap: '1rem',
                     marginRight: '2rem',
-                }
-            }
+                }}
             >
                 <Link href='github.com' color='inherit' underline='none' onClick={() => window.open('https://github.com/dadthegamer')}>
                     <GitHubIcon />
